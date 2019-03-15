d2e04101e6aadfce38523631f7967b896c92387b
Mukund Radhakrishnan <32258999+MukundRadhakrishnan@users.noreply.github.com>
2019-03-15 12:23:53 +0530

    Checking if alphabet or not

---
+++ AlphaCheck.py
@@ -0,0 +1,6 @@
+list=['a','e','i','o','u','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+CodekataExample3=input()
+if(CodekataExample3 in list):
+    print("Alphabet")
+else:
+    print("No")
