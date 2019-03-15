39ab5212f9f7be9e0a8ced16061a4573e3f9d026
Mukund Radhakrishnan <32258999+MukundRadhakrishnan@users.noreply.github.com>
2019-03-15 12:57:12 +0530

    Greates of 3 in python

---
+++ Largestof3CK.py
@@ -0,0 +1,4 @@
+codeKata4_a,codeKata4_b,codeKata4_c = map(int,(input().split()))
+if codeKata4_a>codeKata4_b and codeKata4_a>codeKata4_c : print(codeKata4_a)
+elif codeKata4_b>codeKata4_c : print(codeKata4_b)
+else : print(codeKata4_c)
