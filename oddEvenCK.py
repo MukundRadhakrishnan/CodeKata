759ca955a8f2dc6cc4d1354fadce3b55c825574c
Mukund Radhakrishnan <32258999+MukundRadhakrishnan@users.noreply.github.com>
2019-03-15 10:28:07 +0530

    Printing Odd Or Even

---
+++ oddEvenCK.py
@@ -0,0 +1,5 @@
+CodeKataExample2 = int(input("Enter a number: "))
+if (CodeKataExample2  % 2) == 0:
+   print("{0} is Even".format(CodeKataExample2 ))
+else:
+   print("{0} is Odd".format(CodeKataExample2 ))
