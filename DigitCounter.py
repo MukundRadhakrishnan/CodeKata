1f69fce1fe276a57bd07ccfde15321fc0aba7bcc
Mukund Radhakrishnan <32258999+MukundRadhakrishnan@users.noreply.github.com>
2019-03-15 14:43:00 +0530

    Counting no of digits in py

---
+++ DigitCounter.py
@@ -0,0 +1,7 @@
+CodeKataexample10_n = int(input())
+CodeKataexample10_sum = 0
+CodeKataexample10_a = CodeKataexample10_n
+while CodeKataexample10_a > 0 :
+    CodeKataexample10_sum += 1
+    CodeKataexample10_a //= 10
+print(CodeKataexample10_sum)
